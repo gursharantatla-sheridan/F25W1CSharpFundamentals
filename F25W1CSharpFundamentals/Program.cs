0f7e349e9c4c515623e26f8b02166905a84d56ea
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-05 13:42:40 -0400

    string interpolation

--- F25W1CSharpFundamentals/Program.cs
+++ F25W1CSharpFundamentals/Program.cs
@@ -18,7 +18,19 @@
             decimal mm = 5.5M;
             long l = 435324324L;
 
-            var x = 5.5;
+            var x = 5;
+
+            Console.WriteLine("hello\nworld");
+
+
+
+            int a = 5, b = 3, c = 8;
+
+            // string concatenation
+            Console.WriteLine("a = " + a + ", b = " + b + " and c = " + c);
+
+            // string interpolation
+            Console.WriteLine($"a = {a}, b = {b} and c = {c}");
         }
     }
 }
