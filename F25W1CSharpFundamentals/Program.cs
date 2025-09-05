90d080ca5318e2aadcfcdf58ea3e75b7855efae6
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-05 15:27:01 -0400

    complete

--- F25W1CSharpFundamentals/Program.cs
+++ F25W1CSharpFundamentals/Program.cs
@@ -66,11 +66,45 @@
 
             int nn = Convert.ToInt32(num);
             Console.WriteLine(nn);
+            Console.WriteLine("\n\n\n");
+
+
+
+            double price = 24432.445;
+            Console.WriteLine(price.ToString("C"));
+
+            double rate = 0.12;
+            Console.WriteLine(rate.ToString("P1"));
+
+            double weight = 4332.3573;
+            Console.WriteLine(weight.ToString("F3"));
+
+
+            int? g = null;
+            Console.WriteLine("\n\n\n");
+
+
+
+
+            Person p1 = new Person();
+            p1.name = "John";
+            Person p2 = p1;
+
+            p2.name = "Anne";
+            Console.WriteLine(p1.name);
+
+
+            //int ii = price as int;
         }
     }
 
     class Circle
     {
         public int radius;
+    }
+
+    class Person
+    {
+        public string name;
     }
 }
