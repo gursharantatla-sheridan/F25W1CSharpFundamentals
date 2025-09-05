f47f830d86fd692eb241f2242797289927e0a028
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-05 13:29:41 -0400

    Add project files.

---
+++ F25W1CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace F25W1CSharpFundamentals
+{
+    internal class Program
+    {
+        static int n;
+
+        static void Main(string[] args)
+        {
+            int @double = 5;
+            //int n;
+
+            int m = n + 10;
+
+            int i = 5;
+            double d = 5.5;
+
+            float f = 5.5F;
+            decimal mm = 5.5M;
+            long l = 435324324L;
+
+            var x = 5.5;
+        }
+    }
+}
